5220bca4c45f3e5411c5715e8b872892e38692d8
Pratik Kher <pratikkher1@gmail.com>
2022-08-29 14:34:23 +0530

    First Frontend Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import { MainRoute } from "./Routes/MainRoute";
-
+import Home from './pages/Home'
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="src/Assets//Assets/https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-    <BrowserRouter>
-      <MainRoute />
-  </BrowserRouter>
->>>>>>> 17c05e3bd20d47a70d208091a698828da71ffc54
+    <Home/>
   );
 }
 
