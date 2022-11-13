ba0ce8b6351f34ad221ddb6bcd932f80c57de056
sahildabhi0101 <63159436+sahildabhi0101@users.noreply.github.com>
2022-11-13 13:09:06 +0530

    category done

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useCookies } from 'react-cookie';
 import { useNavigate } from "react-router-dom";
+import CategoryCard from './CategoryCard';
 
 const Categories = () => {
     let navigate = useNavigate();
@@ -14,80 +15,39 @@ const Categories = () => {
     const handleClick1 = () => {
         navigate("/display-projects")
     }
-    return (
-        <section className=" section">
-            {/* <!-- Container Start --> */}
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        {/* <!-- Section title --> */}
-                        <div className="section-title">
-                            <h2>CategoryWise Projects</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, provident!</p>
-                        </div>
-                        <div className="row" style={{ cursor: 'pointer' }}>
-                            {/* <!-- Category list --> */}
-                            <div className="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6" onClick={() => handleClick('html')}>
-                                {/* <a href="categorywiseProject"> */}
-                                <div className="category-block">
-                                    <div className="header">
-                                        <i className="fa fa-laptop icon-bg-1"></i>
-                                        <h4>HTML</h4>
-                                    </div>
-                                    <ul className="category-list">
-                                        <li><a href="category.html">Laptops <span>93</span></a></li>
-                                        <li><a href="category.html">Iphone <span>233</span></a></li>
-                                        <li><a href="category.html">Microsoft <span>183</span></a></li>
-                                        <li><a href="category.html">Monitors <span>343</span></a></li>
-                                    </ul>
+  return (
+    <section className=" section">
+                    {/* <!-- Container Start --> */}
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                {/* <!-- Section title --> */}
+                                <div className="section-title">
+                                    <h2>All Categories</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, provident!</p>
                                 </div>
-                                {/* </a> */}
-                            </div> {/* <!-- /Category List -->
-                                    <!-- Category list --> */}
-                            <div className="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6" onClick={() => handleClick('react')}>
-                                <div className="category-block">
-                                    <div className="header">
-                                        <i className="fa fa-apple icon-bg-2"></i>
-                                        <h4>React</h4>
-                                    </div>
-                                    <ul className="category-list">
-                                        <li><a href="category.html">Cafe <span>393</span></a></li>
-                                        <li><a href="category.html">Fast food <span>23</span></a></li>
-                                        <li><a href="category.html">Restaurants <span>13</span></a></li>
-                                        <li><a href="category.html">Food Track<span>43</span></a></li>
-                                    </ul>
-                                </div>
-                            </div> {/* <!-- /Category List -->
-                                    <!-- Category list --> */}
-                            <div className="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6" onClick={() => handleClick('node')}>
-                                <div className="category-block">
-                                    <div className="header">
-                                        <i className="fa fa-home icon-bg-3"></i>
-                                        <h4>Node</h4>
-                                    </div>
-                                    <ul className="category-list">
-                                        <li><a href="category.html">Farms <span>93</span></a></li>
-                                        <li><a href="category.html">Gym <span>23</span></a></li>
-                                        <li><a href="category.html">Hospitals <span>83</span></a></li>
-                                        <li><a href="category.html">Parolurs <span>33</span></a></li>
-                                    </ul>
-                                </div>
-                            </div> {/* <!-- /Category List -->
-                                    <!-- Category list --> */}
-                            <div className="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
-                                <div className="category-block">
-                                    <div className="header">
-                                        <i className="fa fa-shopping-basket icon-bg-4"></i>
-                                        <h4>Shoppings</h4>
-                                    </div>
-                                    <ul className="category-list">
-                                        <li><a href="category.html">Mens Wears <span>53</span></a></li>
-                                        <li><a href="category.html">Accessories <span>212</span></a></li>
-                                        <li><a href="category.html">Kids Wears <span>133</span></a></li>
-                                        <li><a href="category.html">It & Software <span>143</span></a></li>
-                                    </ul>
-                                </div>
-                            </div> {/* <!-- /Category List -->
+                                <div className="row">
+                                    {/* <!-- Category list --> */}
+
+                                    <CategoryCard onClick={() => handleClick('html')} cat='html'/>
+                                    <CategoryCard onClick={() => handleClick('html')} cat='react'/>
+                                    <CategoryCard onClick={() => handleClick('html')} cat='node'/>
+
+                                    
+                                    <div className="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
+                                        <div className="category-block">
+                                            <div className="header">
+                                                <i className="fa fa-shopping-basket icon-bg-4"></i>
+                                                <h4>Shoppings</h4>
+                                            </div>
+                                            <ul className="category-list">
+                                                <li><a href="category.html">Mens Wears <span>53</span></a></li>
+                                                <li><a href="category.html">Accessories <span>212</span></a></li>
+                                                <li><a href="category.html">Kids Wears <span>133</span></a></li>
+                                                <li><a href="category.html">It & Software <span>143</span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div> {/* <!-- /Category List -->
                                     <!-- Category list --> */}
                             <div className="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                                 <div className="category-block">
