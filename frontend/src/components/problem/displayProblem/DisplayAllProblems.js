d21de4b93cf72474d2e4a06ab3fa8d41d9b9d46e
Jay-1501 <laherijay9348@gmail.com>
2022-11-14 10:41:33 +0530

    last minute changes

--- frontend/src/components/problem/displayProblem/DisplayAllProblems.js
+++ frontend/src/components/problem/displayProblem/DisplayAllProblems.js
@@ -79,6 +79,7 @@ const DisplayAllProblems = () => {
                   // <Link to={`/project/${project._id}`}>
                   <ProblemCardAll
                     key={index}
+                    status={problem.status}
                     problem_id={problem._id}
                     problem_title={problem.problem_title}
                     problem_desc={problem.problem_desc}
@@ -94,6 +95,7 @@ const DisplayAllProblems = () => {
                     // <Link to={`/project/${project._id}`}>
                     <ProblemCardAll
                       key={index}
+                      status={problem.status}
                       problem_id={problem._id}
                       problem_title={problem.problem_title}
                       problem_desc={problem.problem_desc}
